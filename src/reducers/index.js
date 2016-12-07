21f8748997e93bc6f634882b68f2625ac8fbba87
Allan Esquina <loloudxur@gmail.com>
2016-12-07 19:22:31 -0200

    gh-pages

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import TictactoeReducer from './TictactoeReducer';
-
-const rootReducer = combineReducers({
-  TictactoeReducer
-});
-
-export default rootReducer;
